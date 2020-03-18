1e2e4e36b046dcd2c044ed5255d2bb181ea1065a
VirreV <61963500+VirreV@users.noreply.github.com>
2020-03-18 16:49:33 +0100

    Beautified

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,16 @@
-function toggleHmbrgMenu(x) {
-    x.classList.toggle("change");
+let menu = false;
+let darkMode = true;
+function toggleHmbrgMenu() {
+    menu = !(menu);
+    document.querySelector('#container').classList.toggle("change");
     document.querySelector('.dropDownMenu').classList.toggle("open");
+    if(menu){
+        setTimeout(function(){ document.body.classList.toggle("menuOpen"); }, 300);
+    } else{
+        document.body.classList.toggle("menuOpen");
+    }
+}
+
+function toggleDarkMode() {
+    document.querySelector("body").classList.toggle("darkMode");
 }
