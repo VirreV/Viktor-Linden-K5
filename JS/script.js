79437588154a1da892a3678a5cefd2d03235339c
VirreV <61963500+VirreV@users.noreply.github.com>
2020-03-23 08:44:34 +0100

    -fixed the invert problem -started on adding cart

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 let menu = false;
-let darkMode = true;
+let darkMode = false;
 function toggleHmbrgMenu() {
     menu = !(menu);
     document.querySelector('#container').classList.toggle("change");
@@ -21,3 +21,10 @@ function toggleDarkMode() {
         document.querySelector("#colorModeText").innerHTML =  "Light Mode";
     }
 }
+
+document.querySelectorAll(".buy").forEach(el => {
+    el.addEventListener("click", e => {
+        let price = e.target.querySelector("span").innerHTML;
+        
+    })
+})
