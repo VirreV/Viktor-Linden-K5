b14df4da596e86123b6c37fa06a69c8c935e8577
VirreV <61963500+VirreV@users.noreply.github.com>
2020-03-25 01:07:29 +0100

    fixed details and continued with cart menu

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@ let menu = false;
 let cartMenu = false;
 let darkMode = false;
 let cartItems = [];
+let nextCartId = 0;
 
 function toggleHmbrgMenu() {
     menu = !(menu);
@@ -28,25 +29,99 @@ function toggleDarkMode() {
 document.querySelectorAll(".buy").forEach(el => {
     el.addEventListener("click", () => {
         let price = parseInt(el.querySelector(".price").innerHTML.substring(0, el.querySelector(".price").innerHTML.length - 2));
-        let cart = parseInt(document.querySelector("#cartValue").innerHTML);
-        cart += price;
-        document.querySelector("#cartValue").innerHTML = cart;
         let itemName = el.querySelector(".item").innerHTML;
         addCartItem(itemName, price);
+        updateCartPrice();
     })
 })
 
 function addCartItem(itemName, cost){
-    cartItems.push({item: itemName, price: cost});
-    console.log(cartItems);
+    let existing = false;
+    for(let i = 0; i < cartItems.length; i++){
+        console.log(itemName);
+        console.log(cartItems[i].item);
+        if(cartItems[i].item == itemName){
+            existing = true;
+            cartItems[i].count += 1;
+        }
+    }
+    if(!(existing)){
+        cartItems.push({item: itemName, price: cost, count: 1, id: nextCartId});
+        nextCartId++;
+    }
+    updateCartList();
 }
 
 function showCart(){
     cartMenu = !(cartMenu);
     document.querySelector("#cartDiv").classList.toggle("cartOpen");
     if(cartMenu){
-        setTimeout(function(){ document.body.classList.toggle("menuOpen"); }, 300);
+        setTimeout(function(){ document.body.classList.toggle("cartMenuOpen"); }, 300);
     } else{
-        document.body.classList.toggle("menuOpen");
+        document.body.classList.toggle("cartMenuOpen");
     }
+    
+}
+
+function updateCartList(){
+    let x = 0;
+    document.querySelectorAll(".cartItem").forEach(el => {
+        el.remove();
+    });
+
+    for(let i = 0; i < cartItems.length; i++){
+        x++;
+        let newCartItem = document.createElement("li");
+        newCartItem.innerHTML = cartItems[i].item;
+        newCartItem.classList.add("cartItem");
+        let newCartItemCost = document.createElement("span");
+        newCartItemCost.innerHTML = cartItems[i].price + ":-";
+        newCartItem.appendChild(newCartItemCost);
+        let newCartItemCount = document.createElement("div");
+        newCartItemCount.innerHTML = "x" + cartItems[i].count;
+        newCartItemCount.classList.add("cartItemCount");
+        let countAdd = document.createElement("div");
+        countAdd.classList.add("countAdd");
+        let plusSign = document.createElement("div");
+        plusSign.classList.add("plusSign");
+        plusSign.innerHTML = "+";
+        plusSign.addEventListener("click", () => {
+            cartItems[i].count++;
+            updateCartList();
+            updateCartPrice();
+        });
+        let minusSign = document.createElement("div");
+        minusSign.classList.add("minusSign");
+        minusSign.innerHTML = "-";
+        minusSign.addEventListener("click", el => {
+            cartItems[i].count--;
+            if(cartItems[i].count == 0){
+                cartItems.splice(i, 1);
+            }
+            updateCartList();
+            updateCartPrice();
+        });
+        let countRemove = document.createElement("div");
+        countRemove.classList.add("countRemove");
+        countAdd.appendChild(plusSign);
+        countRemove.appendChild(minusSign);
+        newCartItemCount.appendChild(countAdd);
+        newCartItemCount.appendChild(countRemove);
+        newCartItem.appendChild(newCartItemCount);
+        document.querySelector("#cartList").appendChild(newCartItem);
+    }
+    if(x == 0){
+        let emptyCart = document.createElement("div");
+        emptyCart.className = "cartItem";
+        emptyCart.innerHTML = "Your cart is empty";
+        document.querySelector("#cartList").appendChild(emptyCart);
+    }
+}
+
+function updateCartPrice(){
+    let price = 0;
+    for(let i = 0; i < cartItems.length; i++){
+        price += cartItems[i].price * cartItems[i].count;
+    }
+    document.querySelector("#cartValue").innerHTML = price;
 }
